e748d31fb5e910d847a012b0f25032d87cb6f9c5
KazusaTsubota <chachairo.chairo@gmail.com>
2021-10-16 14:35:13 +0900

    既存タグとメモの紐付け

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Memo;
+use App\Models\MemoTag;
+use App\Models\Tag;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -31,7 +34,13 @@ class HomeController extends Controller
             ->orderBy('updated_at', 'DESC') // ASC:小さい順、DESC:大きい順
             ->get();
 
-        return view('create', compact('memos'));
+        $tags = Tag::select('tags.*')
+            ->where('user_id', '=', \Auth::id())
+            ->whereNull('deleted_at')
+            ->orderBy('updated_at', 'DESC')
+            ->get();
+
+        return view('create', compact('memos', 'tags'));
     }
 
     public function store(Request $request)
@@ -39,11 +48,43 @@ class HomeController extends Controller
         $posts = $request->all();
         //dd($posts);
         //dd(\Auth::id());
+        //トランザクション開始
+        //新規タグが入力されているかチェック
+        //新規タグが既にtagsテーブルに存在するかのチェック
+        //新規タグが既に存在しなければ、tagsテーブルにインサート→IDを取得
+        //トランザクション終了
+        DB::transaction(function () use ($posts) {
+            // メモIDをインサートして取得
+            $memo_id = Memo::insertGetId([
+                'content' => $posts['content'],
+                'user_id' => \Auth::id()
+            ]);
+            // タグがTagテーブルに存在するかを取得
+            $tag_exists = Tag::where('user_id', '=', \Auth::id())
+                ->where('name', '=', $posts['new_tag'])
+                ->exists();
+            // 新規タグが入力されている かつ 新規タグがTagテーブルに同じものが存在しない場合
+            // 新規タグをTagテーブルにインサートして、タグIDを取得
+            if (!empty($posts['new_tag']) && !$tag_exists) {
+                $tag_id = Tag::insertGetId([
+                    'user_id' => \Auth::id(),
+                    'name' => $posts['new_tag']
+                ]);
+                // タグが登録されたときはメモとタグを紐付ける
+                MemoTag::insert([
+                    'memo_id' => $memo_id,
+                    'tag_id' => $tag_id
+                ]);
+            }
+            //既存タグを紐付け
+            foreach ($posts['tags'] as $tag) {
+                MemoTag::insert([
+                    'memo_id' => $memo_id,
+                    'tag_id' => $tag
+                ]);
+            }
+        });
 
-        Memo::insert([
-            'content' => $posts['content'],
-            'user_id' => \Auth::id()
-        ]);
 
         return redirect(route('home'));
     }
@@ -57,9 +98,26 @@ class HomeController extends Controller
             ->orderBy('updated_at', 'DESC') // ASC:小さい順、DESC:大きい順
             ->get();
 
-        $edit_memo = Memo::find($id);
+        //$edit_memo = Memo::find($id);
+        $edit_memo = Memo::select('memos.*', 'tags.id AS tag_id')
+            ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
+            ->leftJoin('tags', 'memo_tags.tag_id', '=', 'tags.id')
+            ->where('memos.user_id', '=', \Auth::id())
+            ->where('memos.id', '=', $id)
+            ->whereNull('memos.deleted_at')
+            ->get();
+        $include_tags = [];
+        foreach ($edit_memo as $memo) {
+            array_push($include_tags, $memo['tag_id']);
+        }
 
-        return view('edit', compact('memos', 'edit_memo'));
+        $tags = Tag::select('tags.*')
+            ->where('user_id', '=', \Auth::id())
+            ->whereNull('deleted_at')
+            ->orderBy('updated_at', 'DESC') // ASC:小さい順、DESC:大きい順
+            ->get();
+
+        return view('edit', compact('memos', 'edit_memo', 'include_tags', 'tags'));
     }
 
     public function update(Request $request)
@@ -67,10 +125,35 @@ class HomeController extends Controller
         $posts = $request->all();
         //dd($posts);
         //dd(\Auth::id());
-
-        Memo::where('id', $posts['memo_id'])->update([
-            'content' => $posts['content']
-        ]);
+        DB::transaction(function () use ($posts) {
+            Memo::where('id', $posts['memo_id'])->update([
+                'content' => $posts['content']
+            ]);
+            MemoTag::where('memo_id', '=', $posts['memo_id'])->delete();
+            foreach ($posts['tags'] as $tag) {
+                MemoTag::insert([
+                    'memo_id' => $posts['memo_id'],
+                    'tag_id' => $tag
+                ]);
+            }
+            // タグがTagテーブルに存在するかを取得
+            $tag_exists = Tag::where('user_id', '=', \Auth::id())
+                ->where('name', '=', $posts['new_tag'])
+                ->exists();
+            // 新規タグが入力されている かつ 新規タグがTagテーブルに同じものが存在しない場合
+            // 新規タグをTagテーブルにインサートして、タグIDを取得
+            if (!empty($posts['new_tag']) && !$tag_exists) {
+                $tag_id = Tag::insertGetId([
+                    'user_id' => \Auth::id(),
+                    'name' => $posts['new_tag']
+                ]);
+                // タグが登録されたときはメモとタグを紐付ける
+                MemoTag::insert([
+                    'memo_id' => $posts['memo_id'],
+                    'tag_id' => $tag_id
+                ]);
+            }
+        });
 
         return redirect(route('home'));
     }
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -6,18 +6,27 @@
             メモ編集
             <form class="card-body" action="{{ route('destory') }} " method="POST">
                 @csrf
-                <input type="hidden" name="memo_id" value="{{ $edit_memo['id'] }}">
+                <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}">
                 <button type="submit" class="btn btn-secondary">削除</button>
             </form>
         </div>
         {{-- route('store')とかくと/store --}}
         <form class="card-body" action="{{ route('update') }} " method="POST">
             @csrf
-            <input type="hidden" name="memo_id" value="{{ $edit_memo['id'] }}">
+            <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}">
             <div class="form-group">
                 <textarea class="form-control" name="content" rows="3"
-                    placeholder="ここにメモを入力">{{ $edit_memo['content'] }}</textarea>
+                    placeholder="ここにメモを入力">{{ $edit_memo[0]['content'] }}</textarea>
             </div>
+            @foreach ($tags as $tag)
+                <div class="form-check form-check-inline mb-3">
+                    {{-- 3高演算子 --}}
+                    <input type="checkbox" class="form-check-input" name="tags[]" id="{{ $tag['id'] }}"
+                        value="{{ $tag['id'] }}" {{ in_array($tag['id'], $include_tags) ? 'checked' : '' }}>
+                    <label for="{{ $tag['id'] }}" class="form-check-label">{{ $tag['name'] }}</label>
+                </div>
+            @endforeach
+            <input type="text" class="form-control w-50 mb-3" name="new_tag" placeholder="新しいタグを入力">
             <button type="submit" class="btn btn-primary">更新</button>
         </form>
     </div>
